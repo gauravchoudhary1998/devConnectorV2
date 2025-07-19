c2552698431590341fec2ee8d166a6428339b9dc
Gaurav Choudhary <choudharyg015@gmail.com>
2025-07-19 18:18:25 +0000

    updated react_api_url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,7 +4,7 @@ import { LOGOUT } from '../actions/types';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL || '/api'}`,
+  baseURL: `${process.env.REACT_APP_API_URL || 'http://localhost:5000/api'}`,
   headers: {
     'Content-Type': 'application/json'
   }
