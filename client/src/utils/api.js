45e15c3b5eafbbcc3dc4b0cb6a6facd5a5db5ac6
Gaurav Choudhary <choudharyg015@gmail.com>
2025-07-19 16:58:12 +0000

    added ingress

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,7 +4,7 @@ import { LOGOUT } from '../actions/types';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL || 'http://backend:5000/api'}`,
+  baseURL: `${process.env.REACT_APP_API_URL || '/api'}`,
   headers: {
     'Content-Type': 'application/json'
   }
